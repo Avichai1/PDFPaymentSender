97d4027ba54e7216f390d11bb6b28cfc0949b884
avihai <avihai@HP-AVICHAI>
2021-07-25 10:50:03 +0300

    Add project files.

---
+++ PDFPaymentSender/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿
+namespace PDFPaymentSender
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.choosefilebtn = new System.Windows.Forms.Button();
+            this.FilePathBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.FilePagesNumberBox = new System.Windows.Forms.TextBox();
+            this.SendButton = new System.Windows.Forms.Button();
+            this.UsersManagementList = new System.Windows.Forms.ListView();
+            this.RowNumberCol = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.FullNameCol = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.MailAddressCol = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.IDCol = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.StatusCol = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Filllistbtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Script", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(523, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "שליחת תלושים";
+            // 
+            // choosefilebtn
+            // 
+            this.choosefilebtn.Location = new System.Drawing.Point(1015, 186);
+            this.choosefilebtn.Name = "choosefilebtn";
+            this.choosefilebtn.Size = new System.Drawing.Size(75, 31);
+            this.choosefilebtn.TabIndex = 1;
+            this.choosefilebtn.Text = "בחר קובץ";
+            this.choosefilebtn.UseVisualStyleBackColor = true;
+            this.choosefilebtn.Click += new System.EventHandler(this.choosefilebtn_Click);
+            // 
+            // FilePathBox
+            // 
+            this.FilePathBox.Location = new System.Drawing.Point(741, 193);
+            this.FilePathBox.Name = "FilePathBox";
+            this.FilePathBox.Size = new System.Drawing.Size(250, 20);
+            this.FilePathBox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label2.Location = new System.Drawing.Point(1025, 271);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = ":כמות דפים";
+            // 
+            // FilePagesNumberBox
+            // 
+            this.FilePagesNumberBox.Location = new System.Drawing.Point(972, 268);
+            this.FilePagesNumberBox.Name = "FilePagesNumberBox";
+            this.FilePagesNumberBox.Size = new System.Drawing.Size(49, 20);
+            this.FilePagesNumberBox.TabIndex = 4;
+            // 
+            // SendButton
+            // 
+            this.SendButton.Location = new System.Drawing.Point(541, 551);
+            this.SendButton.Name = "SendButton";
+            this.SendButton.Size = new System.Drawing.Size(75, 23);
+            this.SendButton.TabIndex = 6;
+            this.SendButton.Text = "שלח";
+            this.SendButton.UseVisualStyleBackColor = true;
+            this.SendButton.Click += new System.EventHandler(this.SendButton_Click);
+            // 
+            // UsersManagementList
+            // 
+            this.UsersManagementList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.RowNumberCol,
+            this.FullNameCol,
+            this.MailAddressCol,
+            this.IDCol,
+            this.StatusCol});
+            this.UsersManagementList.HideSelection = false;
+            this.UsersManagementList.Location = new System.Drawing.Point(36, 59);
+            this.UsersManagementList.Name = "UsersManagementList";
+            this.UsersManagementList.Size = new System.Drawing.Size(566, 460);
+            this.UsersManagementList.TabIndex = 7;
+            this.UsersManagementList.UseCompatibleStateImageBehavior = false;
+            this.UsersManagementList.MouseClick += new System.Windows.Forms.MouseEventHandler(this.UsersManagementList_MouseClick);
+            // 
+            // RowNumberCol
+            // 
+            this.RowNumberCol.Text = "מיקום";
+            this.RowNumberCol.Width = 80;
+            // 
+            // FullNameCol
+            // 
+            this.FullNameCol.Text = "שם מלא";
+            this.FullNameCol.Width = 120;
+            // 
+            // MailAddressCol
+            // 
+            this.MailAddressCol.Text = "כתובת מייל";
+            this.MailAddressCol.Width = 150;
+            // 
+            // IDCol
+            // 
+            this.IDCol.Text = "תעודת זהות";
+            this.IDCol.Width = 100;
+            // 
+            // StatusCol
+            // 
+            this.StatusCol.Text = "סטאטוס";
+            // 
+            // Filllistbtn
+            // 
+            this.Filllistbtn.Location = new System.Drawing.Point(763, 361);
+            this.Filllistbtn.Name = "Filllistbtn";
+            this.Filllistbtn.Size = new System.Drawing.Size(139, 23);
+            this.Filllistbtn.TabIndex = 15;
+            this.Filllistbtn.Text = "מלא את הטבלה ";
+            this.Filllistbtn.UseVisualStyleBackColor = true;
+            this.Filllistbtn.Click += new System.EventHandler(this.Filllistbtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1238, 619);
+            this.Controls.Add(this.Filllistbtn);
+            this.Controls.Add(this.UsersManagementList);
+            this.Controls.Add(this.SendButton);
+            this.Controls.Add(this.FilePagesNumberBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.FilePathBox);
+            this.Controls.Add(this.choosefilebtn);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button choosefilebtn;
+        private System.Windows.Forms.TextBox FilePathBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox FilePagesNumberBox;
+        private System.Windows.Forms.Button SendButton;
+        private System.Windows.Forms.ListView UsersManagementList;
+        private System.Windows.Forms.ColumnHeader RowNumberCol;
+        private System.Windows.Forms.ColumnHeader FullNameCol;
+        private System.Windows.Forms.ColumnHeader MailAddressCol;
+        private System.Windows.Forms.ColumnHeader IDCol;
+        private System.Windows.Forms.ColumnHeader StatusCol;
+        private System.Windows.Forms.Button Filllistbtn;
+    }
+}
+
---
+++ PDFPaymentSender/Form1.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using outlook = Microsoft.Office.Interop.Outlook;
+using System.IO;
+using Syncfusion.Pdf;
+using Syncfusion.Pdf.Parsing;
+using Syncfusion.Pdf.Security;
+
+namespace PDFPaymentSender
+{
+    public partial class Form1 : Form
+    {        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            UsersManagementList.View = View.Details;//מציג את הנתונים בעמודות
+            UsersManagementList.GridLines = true;//מציג תצוגה רשתית של קווים
+            UsersManagementList.FullRowSelect = true;//שורה כחולה כאשר עומדים על תא כלשהו בטבלה
+            UsersManagementList.CheckBoxes = true;
+        }
+        string dir = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "PDFPaymentToMail");
+        private void choosefilebtn_Click(object sender, EventArgs e)
+        {            
+            if (!Directory.Exists(dir))
+            {
+                Directory.CreateDirectory(dir);
+            }
+            var file_path = string.Empty;
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "pdf files (*.pdf)|*.pdf|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    file_path = openFileDialog.FileName;
+                    FilePathBox.Text = file_path.ToString();
+                    PdfLoadedDocument reader = new PdfLoadedDocument(file_path.ToString());
+                    FilePagesNumberBox.Text = reader.Pages.Count.ToString();
+                }
+            }           
+        }
+        bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        private void Filllistbtn_Click(object sender, EventArgs e)
+        {
+            UsersManagementList.Items.Clear();
+            string textfile_path = Path.Combine(dir, "pdfread.txt");            
+            if (FilePathBox.Text != string.Empty)
+            {
+                if (File.Exists(textfile_path))
+                {
+                    File.Delete(textfile_path);
+                }
+                PdfLoadedDocument reader = new PdfLoadedDocument(FilePathBox.Text);
+                PdfLoadedPageCollection pages = reader.Pages;
+                for (int i = 0; i < pages.Count ; i++)
+                {                    
+                    PdfPageBase page = pages[i];
+                    string extractedTexts = page.ExtractText(true);
+                    Char[] tempArray = extractedTexts.ToCharArray();
+                    Array.Reverse(tempArray);
+                    extractedTexts = new string(tempArray);
+                    string strText = string.Empty;
+                    extractedTexts = System.Text.Encoding.UTF8.GetString(System.Text.ASCIIEncoding.Convert(System.Text.Encoding.Default, System.Text.Encoding.UTF8, System.Text.Encoding.Default.GetBytes(extractedTexts)));
+                    strText = strText + extractedTexts.Replace("  ", "-");
+                    string strText_second_level = string.Empty;
+                    strText = System.Text.Encoding.UTF8.GetString(System.Text.ASCIIEncoding.Convert(System.Text.Encoding.Default, System.Text.Encoding.UTF8, System.Text.Encoding.Default.GetBytes(strText)));
+                    strText_second_level = strText_second_level + strText.Replace(" ", string.Empty);
+                    string strText_last_level = string.Empty;
+                    strText_second_level = System.Text.Encoding.UTF8.GetString(System.Text.ASCIIEncoding.Convert(System.Text.Encoding.Default, System.Text.Encoding.UTF8, System.Text.Encoding.Default.GetBytes(strText_second_level)));
+                    strText_last_level = strText_last_level + strText_second_level.Replace("-", " ");
+                    using (FileStream fs = File.Create(textfile_path))
+                    {
+                        // Add some text to file    
+                        byte[] pdftotext = new UTF8Encoding(true).GetBytes(strText_last_level);
+                        fs.Write(pdftotext, 0, pdftotext.Length);
+                    }
+                    if (File.Exists(textfile_path))
+                    {
+                        string[] lines = File.ReadAllLines(textfile_path);
+                        var lineCount = File.ReadLines(textfile_path).Count();
+                        string Corrupt_ID = lines[lineCount - 15];                                              
+                        string Remove_corrupt_id_spaces = Corrupt_ID.Replace(" ", string.Empty);
+                        string remove_corrupt_id_last_chrachter = Remove_corrupt_id_spaces.Remove(Remove_corrupt_id_spaces.Length - 1, 1);
+                        char[] temparray_with_double_zero = remove_corrupt_id_last_chrachter.ToCharArray();
+                        string ID_Remove_Double_Zero = null;
+                        if (temparray_with_double_zero[0].ToString() == "0" && temparray_with_double_zero[1].ToString() == "0")
+                        {
+                            ID_Remove_Double_Zero = remove_corrupt_id_last_chrachter.Remove(1, remove_corrupt_id_last_chrachter.Length - 1);
+                        }
+                        else
+                        {
+                            ID_Remove_Double_Zero = remove_corrupt_id_last_chrachter;
+                        }
+                        char[] temparray = ID_Remove_Double_Zero.ToCharArray();
+                        Array.Reverse(temparray);
+                        string ID_Check_If_101_End = new string(temparray);
+                        string ID = null;
+                        if (ID_Check_If_101_End.Length > 9)
+                        {
+                            ID = ID_Check_If_101_End.Substring(0, ID_Check_If_101_End.Length - 3);
+                            ID = "2" + ID;
+                        }
+                        else
+                        {
+                            ID = ID_Check_If_101_End;
+                        }
+                        string full_name = lines[lineCount - 5];
+                        string mail_address = "";
+                        string[] row = { (UsersManagementList.Items.Count + 1).ToString(), full_name, mail_address, ID };
+                        var listViewItem = new ListViewItem(row);
+                        UsersManagementList.Items.Add(listViewItem);
+                    }                    
+                }
+            }
+            else
+            {
+                MessageBox.Show("עליך לבחור קובץ כדי למלא את הטבלה", "שגיאה", MessageBoxButtons.OK);
+            }
+        }
+
+        private void UsersManagementList_MouseDown(object sender, MouseEventArgs e)
+        {
+
+        }
+        TextBox txtBox1 = new TextBox();
+        private int subItemIndex = 0;
+        private ListViewItem viewItem;
+
+        private int? xpos = null;
+        public Form1()
+        {
+            InitializeComponent();
+            UsersManagementList.Controls.Add(txtBox1);
+            txtBox1.Visible = false;
+            txtBox1.KeyPress += (sender, args) =>
+            {
+                TextBox textBox = sender as TextBox;
+
+                if ((int)args.KeyChar == 13)
+                {
+                    if (viewItem != null && IsValidEmail(txtBox1.Text) == true)
+                    {
+                        viewItem.SubItems[subItemIndex].Text = textBox.Text;
+                    }
+                    else
+                    {
+                        MessageBox.Show("שדה כתובת מייל ריק או שלא הוזן נכון", "שגיאה", MessageBoxButtons.OK);
+                    }
+                    textBox.Visible = false;
+                }
+                else
+                {
+                    textBox.Visible = true;
+                }
+            };
+        }
+        private void UsersManagementList_MouseClick(object sender, MouseEventArgs e)
+        {
+            xpos = MousePosition.X - UsersManagementList.PointToScreen(Point.Empty).X;
+            if (e.Button == MouseButtons.Left && UsersManagementList.SelectedItems.Count > 0)
+            {
+                viewItem = UsersManagementList.SelectedItems[0];
+                var bounds = viewItem.Bounds;
+                var col3_bounds = viewItem.SubItems[2].Bounds;
+
+                if (xpos > col3_bounds.X)
+                {
+                    subItemIndex = 2;
+                    txtBox1.SetBounds(col3_bounds.X, bounds.Y, col3_bounds.Width, bounds.Height);
+                }
+                txtBox1.Text = string.Empty;
+                txtBox1.Visible = true;                                
+            }
+        }
+        static void pdftextreader(string Path, string ID, string Mail_Recipent, string Name)
+        {
+            string dir = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "PDFPaymentToMail");
+            if (!Directory.Exists(dir))
+            {
+                Directory.CreateDirectory(dir);
+            }
+            string path_for_the_folder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData).ToString() + "\\PDFPaymentToMail";
+            PdfLoadedDocument reader = new PdfLoadedDocument(Path);
+            PdfLoadedPageCollection loadedPages = reader.Pages;
+            int pageIndex = -1;
+            string text = string.Empty;
+            string ID_Adding_Makaf_In_Center = ID.Insert(8, "-");
+            for (int i = 1; i < loadedPages.Count; i++)
+            {
+                PdfPageBase loadedPage = loadedPages[i];
+                text = loadedPage.ExtractText();
+                if (text.Contains(ID_Adding_Makaf_In_Center))
+                {
+                    pageIndex = i;
+                    break;
+                }
+            }
+            if (pageIndex != -1)
+            {                
+                Syncfusion.Pdf.PdfDocument document = new Syncfusion.Pdf.PdfDocument();
+                document.ImportPage(reader, pageIndex);
+                string file_name = Name + ".pdf";
+                PdfSecurity security = document.Security;
+                security.KeySize = PdfEncryptionKeySize.Key256BitRevision6;
+                security.Algorithm = PdfEncryptionAlgorithm.AES;
+                security.Permissions = PdfPermissionsFlags.Print | PdfPermissionsFlags.FullQualityPrint;
+                security.OwnerPassword = ID;
+                security.UserPassword = ID;
+                string path_for_saving_the_pdffile = System.IO.Path.Combine(path_for_the_folder, file_name);
+                document.Save(path_for_saving_the_pdffile);
+                document.Close(true);
+                reader.Close(true);
+                dynamic app = Activator.CreateInstance(Type.GetTypeFromProgID("Outlook.Application"));
+                dynamic email = app.CreateItem(0);
+                email.Subject = "תלוש שכר";
+                email.Body = "תלוש שכר";
+                email.To = Mail_Recipent;
+                email.Save();
+                email.Attachments.Add(path_for_saving_the_pdffile);
+                email.send();
+            }
+            else
+            {
+                MessageBox.Show("קובץ לא נמצא", "שגיאה", MessageBoxButtons.OK);
+            }
+        }
+        private void SendButton_Click(object sender, EventArgs e)
+        {
+            string send = string.Empty;
+            for (int i = 0; i < UsersManagementList.Items.Count; i++)
+            {
+                if (UsersManagementList.Items[i].SubItems[1].Text == string.Empty)
+                {
+                    MessageBox.Show(UsersManagementList.Items[i].SubItems[1]+"חסר שם מלא בשורה", "שגיאה", MessageBoxButtons.OK);
+                    break;
+                }
+                else if(UsersManagementList.Items[i].SubItems[2].Text == string.Empty)
+                {
+                    MessageBox.Show(UsersManagementList.Items[i].SubItems[2] + "חסרה כתובת מייל בשורה", "שגיאה", MessageBoxButtons.OK);
+                    break;
+                }
+                else if (UsersManagementList.Items[i].SubItems[3].Text == string.Empty)
+                {
+                    MessageBox.Show(UsersManagementList.Items[i].SubItems[3] + "חסרה תעודת זהות בשורה", "שגיאה", MessageBoxButtons.OK);
+                    break;
+                }
+                else
+                {
+                    send = "yes";
+                }
+            }
+            if (send=="yes")
+            {
+                for (int i = 0; i < UsersManagementList.Items.Count; i++)
+                {
+                    pdftextreader(FilePathBox.Text, UsersManagementList.Items[i].SubItems[3].Text, UsersManagementList.Items[i].SubItems[2].Text, UsersManagementList.Items[i].SubItems[1].Text);
+                    UsersManagementList.Items[i].SubItems[4].Text = "נשלח";
+                }
+            }
+        }
+    }
+}
